330129a2cc383fd4a01811814f03266c6655dfd8
Usman <usys537@yahoo.com>
2013-05-11 21:50:07 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gombestatejudiciary::Application.config.secret_token = '1a610f9421c0a404b219b91a5fc2146ec6947d0488ce4b67f5976e9a05909b229ea21a7aa44152aefaea62152f8ed7f6abd191df41a6eb5393b79871b0764319'
