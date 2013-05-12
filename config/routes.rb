2d67193e3e1c6f21d42990111eaa9ba7b2503c88
Usman <usys537@yahoo.com>
2013-05-12 20:07:37 +0100

    Added Bootsrap and layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Gombestatejudiciary::Application.routes.draw do
+
+  root to: 'home#index'
+  get 'pages/about-us' => 'high_voltage/pages#show', :id => 'about-us'
+  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
+
+  
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -48,7 +55,7 @@ Gombestatejudiciary::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
