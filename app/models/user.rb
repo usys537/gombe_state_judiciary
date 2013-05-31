84584b53e1209f9c5e9f258878ca9e48ede0d501
Usman <usys537@yahoo.com>
2013-05-31 22:53:36 +0100

    Overided to_s method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,22 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me,
                   :first_name, :last_name, :profile_name
   # attr_accessible :title, :body
+  validates :first_name, presence: true
+
+  validates :last_name, presence: true
+
+  validates :profile_name, presence: true,
+                           uniqueness: true
+
+  def full_name
+  	first_name + " " + last_name
+  end
+
+  def to_param
+    profile_name
+  end
+
+  def to_s
+    profile_name
+  end
 end
